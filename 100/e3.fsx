7b49e4f44c77fe973c439f35954e92f942973a4a
ideaflare <11017081+ideaflare@users.noreply.github.com>
2017-11-02 17:44:11 +0200

    3.1

--- 100/e3.fsx
+++ 100/e3.fsx
@@ -1,8 +1,11 @@
-#load "../lib/prime.fsx"
-
-let largestFactor n =
-    Prime.factors n
-    |> List.max
+let largestFactor num =
+    let rec cut prime num =
+        match num % prime = 0L with
+        | true -> cut prime (num / prime)
+        | false -> 
+            if num <= prime then prime
+            else cut (prime + 1L) num
+    cut 2L num
 
 printfn "test 13195 (29) = %A" (largestFactor 13195L)
 printfn "real = %A" (largestFactor 600851475143L)
