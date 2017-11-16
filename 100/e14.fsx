aa1c973c72455d8500909229e7509a7115517ee7
ideaflare <11017081+ideaflare@users.noreply.github.com>
2017-11-16 08:59:43 +0200

    14.2

--- 100/e14.fsx
+++ 100/e14.fsx
@@ -1,16 +1,39 @@
 let nextChainElement n =
     match n with
-    | even when n % 2I = 0I -> even / 2I
-    | odd -> (3I * odd) + 1I
+    | even when n % 2L = 0L -> even / 2L
+    | odd -> (3L * odd) + 1L
+
+// ------------------------------------------------------------------------
 
 let maxByCollatzChain searchRange =
     let lookup = System.Collections.Concurrent.ConcurrentDictionary()
-    lookup.GetOrAdd(1I,1I) |> ignore
+    lookup.GetOrAdd(1L,1L) |> ignore
     let rec newCollatzLength i =
-        1I + lookup.GetOrAdd(nextChainElement i, newCollatzLength)        
+        1L + lookup.GetOrAdd(nextChainElement i, newCollatzLength)        
     let collatzLength x = lookup.GetOrAdd(x, newCollatzLength)
     searchRange
     |> List.maxBy collatzLength
 
-printfn "test 13 (10) = %A" (maxByCollatzChain [13I])
-printfn "real = %A" (maxByCollatzChain [2I..999999I])
+
+printfn "real = %A" (maxByCollatzChain [1L..999999L]) // time 1.87s
+// ------------------------------------------------------------------------
+
+let collatzLength n =
+    let rec c' n acc =
+        1L + match n with
+             | 1L -> acc
+             | _ -> c' (nextChainElement n) acc
+    c' n 0L
+
+printfn "test 13 (10) = %A" (collatzLength 13L)
+printfn "real = %A" ([1L..999999L] |> List.maxBy collatzLength) // time 1.61 (seq isn't much faster in this case)
+
+// ------------------------------------------------------------------------
+
+let parallelSerch =
+    [|1L..999999L|]
+    |> Array.Parallel.map (fun t -> async { return t, collatzLength t })
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> Array.reduce (fun (n, collatz) (n2, collatz2) -> if collatz > collatz2 then (n,collatz) else (n2,collatz2) )
+    // Slowest, 2.5s (parallel overhead)
