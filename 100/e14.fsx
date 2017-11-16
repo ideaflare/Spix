c7d5738cbab97f0ace2c893d6ad30af8cce62b47
ideaflare <11017081+ideaflare@users.noreply.github.com>
2017-11-16 07:54:03 +0200

    14.1

--- 100/e14.fsx
+++ 100/e14.fsx
@@ -2,26 +2,15 @@ let nextChainElement n =
     match n with
     | even when n % 2I = 0I -> even / 2I
     | odd -> (3I * odd) + 1I
-    
-let memoize (f: 'a -> 'b) =
-    let lookup = System.Collections.Concurrent.ConcurrentDictionary<'a,'b>()
-    fun n -> lookup.GetOrAdd(n,f)
 
-let collatzChainLength n =
+let maxByCollatzChain searchRange =
     let lookup = System.Collections.Concurrent.ConcurrentDictionary()
     lookup.GetOrAdd(1I,1I) |> ignore
-    let rec getCollatzLength n =
-        let nextChainItem = nextChainElement n
-        1I + lookup.GetOrAdd(nextChainItem, getCollatzLength)        
-    lookup.GetOrAdd(n, getCollatzLength)
+    let rec newCollatzLength i =
+        1I + lookup.GetOrAdd(nextChainElement i, newCollatzLength)        
+    let collatzLength x = lookup.GetOrAdd(x, newCollatzLength)
+    searchRange
+    |> List.maxBy collatzLength
 
-let longestChainUnder n =
-    [1I..n]
-    |> List.rev
-    |> List.skip 1
-    |> List.map collatzChainLength
-    |> List.max
-
-
-printfn "test 13 (10) = %A" (collatzChainLength 13I)
-printfn "real = %A" (longestChainUnder 999999I)
+printfn "test 13 (10) = %A" (maxByCollatzChain [13I])
+printfn "real = %A" (maxByCollatzChain [2I..999999I])
