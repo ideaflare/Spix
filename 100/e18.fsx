f4af9532a6d2e11067ece8a58e1f84532a137e2c
ideaflare <11017081+ideaflare@users.noreply.github.com>
2017-12-10 14:50:55 +0200

    18.2

--- 100/e18.fsx
+++ 100/e18.fsx
@@ -3,17 +3,15 @@ let data = ScriptData.dataRows __SOURCE_FILE__ __SOURCE_DIRECTORY__
 
 let upsideDownTriangle = ScriptData.parseGrid data |> List.rev
 
-let rec sumMaxParentsDown (upsideDownTriangle: int list list) =
-    match upsideDownTriangle with
-    | [[maxPathSum]] -> maxPathSum
-    | topRow :: children :: restOfTriangle ->
-        let childrenAddMaxParent =
-            topRow
+let sumMaxParentsDown (upsideDownTriangle: int list list) =
+    let childrenAddMaxParent parents children =
+            parents
             |> List.windowed 2
             |> List.map (List.max)
             |> List.zip children
             |> List.map (fun (child, maxParent) -> child + maxParent)
-        sumMaxParentsDown (childrenAddMaxParent :: restOfTriangle)
-    | _ -> 0
+    upsideDownTriangle
+    |> List.reduce childrenAddMaxParent
+    |> List.sum
 
 printfn "real = %A" (sumMaxParentsDown upsideDownTriangle)
