6021488562f2362a94ecb51e424ae2f950771257
ideaflare <11017081+ideaflare@users.noreply.github.com>
2018-01-10 17:16:58 +0200

    9.1

--- 100/e9.fsx
+++ 100/e9.fsx
@@ -1,12 +1,9 @@
-let cartesian xs ys = 
-    xs |> List.collect (fun x -> ys |> List.map (fun y -> x, y))
-
 let isPythagoreanTriplet (a,b,c) =    
     (a < b && b < c) && (a * a + b * b = c * c)
 
 let tripletProduct sum =
-    cartesian [1..sum] [1..sum]
-    |> Seq.map (fun (a,b) -> a, b, (sum - a - b))
+    [1..sum]
+    |> List.collect (fun a -> [a..sum] |> List.map (fun b -> a,b, sum - a - b))
     |> Seq.find isPythagoreanTriplet
     |> (fun (a,b,c) -> a * b * c)
 
