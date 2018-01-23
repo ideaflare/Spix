4070cb4deb6101805deef5292a09ed87cdaf23ad
ideaflare <11017081+ideaflare@users.noreply.github.com>
2018-01-23 22:05:07 +0200

    62.1

--- 100/e62.fsx
+++ 100/e62.fsx
@@ -1,24 +1,20 @@
-let digits = string >> Seq.map (string >> System.Int32.Parse)
+type Big = System.Numerics.BigInteger
 
-let sortedDigits (b: System.Numerics.BigInteger) =  b |> digits |> Seq.sort |> List.ofSeq
+let digits (b:Big) = b |> string |> Seq.map (string >> System.Int32.Parse)
 
-let cube (n:System.Numerics.BigInteger) = n * n * n
+let sortedDigits = digits >> Seq.sort >> List.ofSeq
+
+let cube (n:Big) = n * n * n
 
 let smallestCube nPermutations =
     let rec sc i (lookup : Map<int list,System.Numerics.BigInteger list>) =
         let iCube = cube i
         let key = sortedDigits iCube
-        match lookup.TryFind(key) with
-        | Some(members) ->
-            let newMembers = iCube :: members
-            if List.length newMembers = nPermutations
-            then Seq.min members
-            else
-                let newLookup = lookup.Remove(key).Add(key,newMembers)
-                sc (i + 1I) newLookup
-        | None ->
-            sc (i + 1I) (lookup.Add(key,[iCube]))
-    sc 1I Map.empty
+        let members = iCube :: (defaultArg (lookup.TryFind(key)) [])
+        if members.Length = nPermutations
+        then List.min members
+        else sc (i + Big.One) (lookup.Remove(key).Add(key,members))
+    sc Big.One Map.empty
 
 printfn "test smallestCube 3 permutations (41063625) = %A" (smallestCube 3)
 printfn "real = %A" (smallestCube 5)
