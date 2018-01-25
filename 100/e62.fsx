3ea9d7bbeeef40df3a749aec3ea920115b3f029c
ideaflare <11017081+ideaflare@users.noreply.github.com>
2018-01-25 04:13:36 +0200

    62 : Remove cube function and type re-declaration

--- 100/e62.fsx
+++ 100/e62.fsx
@@ -1,20 +1,16 @@
-type Big = System.Numerics.BigInteger
-
-let digits (b:Big) = b |> string |> Seq.map (string >> System.Int32.Parse)
+let digits (b:System.Numerics.BigInteger) = b |> string |> Seq.map (string >> System.Int32.Parse)
 
 let sortedDigits = digits >> Seq.sort >> List.ofSeq
 
-let cube (n:Big) = n * n * n
-
 let smallestCube nPermutations =
     let rec sc i (lookup : Map<int list,System.Numerics.BigInteger list>) =
-        let iCube = cube i
-        let key = sortedDigits iCube
-        let members = iCube :: (defaultArg (lookup.TryFind(key)) [])
+        let cube = i * i * i
+        let key = sortedDigits cube
+        let members = cube :: (defaultArg (lookup.TryFind(key)) [])
         if members.Length = nPermutations
         then List.min members
-        else sc (i + Big.One) (lookup.Remove(key).Add(key,members))
-    sc Big.One Map.empty
+        else sc (i + 1I) (lookup.Remove(key).Add(key,members))
+    sc 1I Map.empty
 
 printfn "test smallestCube 3 permutations (41063625) = %A" (smallestCube 3)
 printfn "real = %A" (smallestCube 5)
