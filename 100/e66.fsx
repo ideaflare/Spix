4db320657378c066dcc40b3ca668bac515047ef4
ideaflare <11017081+ideaflare@users.noreply.github.com>
2018-02-03 17:26:32 +0200

    66

--- 100/e66.fsx
+++ 100/e66.fsx
@@ -3,40 +3,6 @@
 type Big = System.Numerics.BigInteger
 
 let bigSquare = fun b -> Big.Pow(b,2)
-<<<<<<< HEAD
-
-let rootPow b = (bigSquare b, b)
-
-#load "../lib/bigHelpers.fsx"
-let bigSqrt = BigHelpers.maybeSquare
-
- // x^2 - D.y^2 = 1
- // x^2 - 1 = d.y^2
- // (x^2 - 1)/d = y^2
-
-let hasYSolution d x =
-    let lhs x y = (bigSquare x) - (d * (bigSquare y))
-    let xSquareMin1 = (bigSquare x) - 1I
-    let ySquared = xSquareMin1 / d
-    if ySquared > 0I && xSquareMin1 = d * ySquared
-    then
-        // printfn "ySquared %A" ySquared
-        match bigSqrt ySquared with
-        | Some(y)  -> 
-            // printfn "y %A" y
-            true
-        | _ -> false
-    else false
-
-let findX d =
-    let rec fx' d x =
-        if hasYSolution d x 
-        then
-            printfn "D[%A] x %A" d x
-            (x,d)
-        else fx' d (x + 1I)
-    fx' d 1I
-=======
 let lhs d x y = (bigSquare x) - (d * (bigSquare y))
 
 type Rational = { Numerator : Big; Denominator : Big} with
@@ -71,7 +37,6 @@ let findX d =
     sqrtFractionalExpansion d
     |> Seq.find (fun r -> bigSquare(r.Numerator) - d * bigSquare(r.Denominator) = 1I)
     |> (fun r -> d, r.Numerator)
->>>>>>> 66
 
 let isSquare n =
     let intRoot = int (sqrt (float n))
@@ -81,16 +46,7 @@ let largestMinimalX dMax =
     [2..dMax]
     |> List.filter (isSquare >> not)
     |> List.map (Big >> findX)
-<<<<<<< HEAD
-    |> List.maxBy (fun (x,d) -> x)
-    |> (fun (x,d) -> d)
-
-#time
-printfn "real = %A" (largestMinimalX 7)
-=======
     |> List.maxBy (fun (d,x) -> x)
     |> (fun (d,_) -> d)
 
-#time
 printfn "real = %A" (largestMinimalX 1000)
->>>>>>> 66
