89ba99568219ca710f481cb0a600197946bc95e8
ideaflare <11017081+ideaflare@users.noreply.github.com>
2018-03-04 14:21:58 +0200

    70

--- 100/e70.fsx
+++ 100/e70.fsx
@@ -2,39 +2,37 @@
 
 let maxProduct = 1e7 |> int64
 
-let maxRoot = maxProduct |> float |> sqrt |> ceil |> int64
-
-let primesBelowMaxRoot =
+let pairsBelowMax =
+    let maxRoot = maxProduct |> float |> sqrt |> ceil |> int64
     Prime.sequence
     |> Seq.takeWhile (fun p -> p <= maxRoot)
-
-let pairsBelowMax =
-    primesBelowMaxRoot
     |> Seq.collect (fun p1 ->
-        primesBelowMaxRoot
-        |> Seq.skipWhile (fun p2 -> p2 <= p1)
-        |> Seq.map (fun p2 -> (p1,p2))
+        Prime.sequence
+        |> Seq.map (fun p2 -> p1 * p2, p1, p2)
+        |> Seq.takeWhile (fun (prod, _, _) -> prod <= maxProduct)
     )
 
-let pairPhi a b = (a - 1L) * (b - 1L)
 let permutation a b =
     let aTxt = string a |> Seq.sort |> Seq.toList
     let bTxt = string b |> Seq.sort |> Seq.toList
     aTxt = bTxt
 
-let pairPhiIsPermutatoin (a,b) =
-    let product = a * b
+let pairPhi a b = (a - 1L) * (b - 1L)
+
+let pairWithPermutationPhi (product,a,b) =
     let phi = pairPhi a b
     if permutation product phi
     then Some (product,phi) else None
 
-let ratio (n,phi) = (float n) / (float phi)
+let ratio ((n,phi) : int64 * int64) : decimal =
+    (decimal n) / (decimal phi)
 
 let minNByPhiRatio =
     pairsBelowMax
-    |> Seq.choose pairPhiIsPermutatoin
-    |> Seq.minBy ratio
+    |> Seq.choose pairWithPermutationPhi
+    |> Seq.sortByDescending ratio
+    |> Seq.last
     |> fst
 
-printfn "real = %A" minNByPhiRatio
+printfn "real = %i" minNByPhiRatio
 
