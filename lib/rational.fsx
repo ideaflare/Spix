c24570a35241654a30becdf5bc61064c9ab4db7b
ideaflare <11017081+ideaflare@users.noreply.github.com>
2018-03-07 12:20:50 +0200

    Add simplify, comparison & print functions.

--- lib/rational.fsx
+++ lib/rational.fsx
@@ -2,6 +2,14 @@ type Big = System.Numerics.BigInteger
 
 type Rational = { Numerator : Big; Denominator : Big }
 
+let rec gcd a b =
+    if b = 0I then a
+    else gcd b (a % b)
+
+let simplify (r:Rational) =
+    let c' = gcd r.Numerator r.Denominator
+    { Numerator = r.Numerator / c'; Denominator = r.Denominator / c' }
+
 let (/.) n d = { Numerator = n; Denominator = d }
 
 let add r1 r2 = { Numerator = r1.Numerator * r2.Denominator + r2.Numerator * r1.Denominator;
@@ -9,4 +17,9 @@ let add r1 r2 = { Numerator = r1.Numerator * r2.Denominator + r2.Numerator * r1.
 
 let inverse rational = { Numerator = rational.Denominator; Denominator = rational.Numerator }
 
-let print (r:Rational) = sprintf "%A/%A" r.Numerator r.Denominator
+let compare r1 r2 =
+    let n1' = r1.Numerator * r2.Denominator
+    let n2' = r2.Numerator * r1.Denominator
+    Big.Compare(n1',n2')
+
+let print = simplify >> (fun r -> sprintf "%A/%A" r.Numerator r.Denominator)
