015a2475bce626bb87334a57f045e947ab46fea4
ideaflare <11017081+ideaflare@users.noreply.github.com>
2018-04-02 22:21:04 +0200

    Add comparison to Rational type for built-in functions (max, sort, ..)

--- 100/e71.fsx
+++ 100/e71.fsx
@@ -5,10 +5,9 @@ let leftOf3over7 denominatorMax =
     let t = 3I /. 7I
     { 2I .. denominatorMax }
     |> Seq.map (fun d -> ((t.Numerator * d) / t.Denominator) /. d)
-    |> Seq.distinct
-    |> Seq.filter (fun r -> (compare r t) <> 0)
-    |> Seq.sortWith compare
-    |> (Seq.last >> (fun r -> r.Numerator))
+    |> Seq.filter (fun r -> (r <> t))
+    |> Seq.max
+    |> (fun r -> r.Numerator)
 
 printfn "test 8 (2) = %A" (leftOf3over7 8I)
 printfn "real = %A" (leftOf3over7 (1e6 |> Big))
--- lib/rational.fsx
+++ lib/rational.fsx
@@ -1,14 +1,35 @@
 type Big = System.Numerics.BigInteger
 
-type Rational = { Numerator : Big; Denominator : Big }
-
 let rec gcd a b =
     if b = 0I then a
     else gcd b (a % b)
 
-let simplify (r:Rational) =
-    let c' = gcd r.Numerator r.Denominator
-    { Numerator = r.Numerator / c'; Denominator = r.Denominator / c' }
+[<CustomEquality;CustomComparison>]
+type Rational =
+    { Numerator : Big; Denominator : Big }
+    member me.Simplify =
+        let c' = gcd me.Numerator me.Denominator
+        { Numerator = me.Numerator / c'; Denominator = me.Denominator / c' }
+    static member Compare (r1, r2) =
+        let n1' = r1.Numerator * r2.Denominator
+        let n2' = r2.Numerator * r1.Denominator
+        Big.Compare(n1',n2')
+    static member Equal (r1, r2) = Rational.Compare(r1,r2) = 0
+    override r1.Equals(obj) = 
+        match obj with
+        | :? Rational as r2 -> Rational.Equal(r1,r2)
+        | _ -> false
+    override me.GetHashCode() = 17 * me.Numerator.GetHashCode() * me.Denominator.GetHashCode()
+    override me.ToString() = sprintf "%A/%A" me.Numerator me.Denominator
+    interface System.IEquatable<Rational> with
+        member r1.Equals(r2) = Rational.Equal(r1,r2)
+    interface System.IComparable<Rational> with
+        member r1.CompareTo(r2) = Rational.Compare(r1,r2)
+    interface System.IComparable with
+        member r1.CompareTo(obj) =
+            match obj with
+            | :? Rational as r2 -> Rational.Compare(r1,r2)
+            | _ -> 1
 
 let (/.) n d = { Numerator = n; Denominator = d }
 
@@ -16,10 +37,3 @@ let add r1 r2 = { Numerator = r1.Numerator * r2.Denominator + r2.Numerator * r1.
                   Denominator = r1.Denominator * r2.Denominator }
 
 let inverse rational = { Numerator = rational.Denominator; Denominator = rational.Numerator }
-
-let compare r1 r2 =
-    let n1' = r1.Numerator * r2.Denominator
-    let n2' = r2.Numerator * r1.Denominator
-    Big.Compare(n1',n2')
-
-let print = simplify >> (fun r -> sprintf "%A/%A" r.Numerator r.Denominator)
